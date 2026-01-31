22aa8eed58edbca0d7e7f28cae407aae747045d5
Julian David Mejia Otalvaro <69063283+julimejia@users.noreply.github.com>
2026-01-30 20:22:50 -0500

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -23,7 +23,7 @@ def analizar_con_ia(api_key, context_data, user_question):
         """
         
         completion = client.chat.completions.create(
-            model="llama-3.3-70b-specdec",
+            model="llama-3.3-70b-versatile",
             messages=[{"role": "user", "content": prompt}],
             temperature=0.5,
         )
